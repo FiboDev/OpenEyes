ae2010060065e9db6d8c51e61d41f9f2b38ee1ae
juandpadilla@uninorte.edu.co <juandpadilla@uninorte.edu.co>
2022-04-25 21:26:27 -0500

    asd

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -36,7 +36,7 @@ function onLocationFound(e) {
 
 // ############################################### speech ###############################################
 
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+//var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
 var speaker = new SpeechSynthesisUtterance();
